9e1190f0b65a54d6e1b4ba85ef71eeff4e41e3c1
Brayden Cowell <bcowell@hotmail.ca>
2020-04-22 16:13:49 -0400

    Remove snapshot tests

--- src/Dialog/DialogCentreScreen.tsx
+++ src/Dialog/DialogCentreScreen.tsx
@@ -31,8 +31,6 @@ const StyledDialog = styled(Dialog)`
   position: fixed;
   top: 50%;
   left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
   justify-content: center;
   align-items: center;
